7df1c82c4c539b7c9ee0c314fbd0a62d1cb54d6b
Gideon-Pan <s00971052s@gmail.com>
2023-03-15 17:55:01 +0800

    feat: remove comments

--- main.go
+++ main.go
@@ -8,12 +8,7 @@ import (
 
 func main() {
 	cache := localcache.New(nil)
-
-	// set a key/value pair in the cache
 	cache.Set("foo", 123)
-
-	// retrieve a value from the cache by key
 	value := cache.Get("foo")
-
-	fmt.Println(value) // output: "bar"
+	fmt.Println(value)
 }
